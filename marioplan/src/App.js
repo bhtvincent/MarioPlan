40abd94315eb3d97dbd806bec589b76d09c30ed9
Vincent Tran <bhtbi@yahoo.com>
2022-01-18 23:44:00 -0800

    Added Create Project Page

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/layout/Navbar"
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails';
-
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
 
 /* 
   Route means when user loads '/' then load in Dashboard inside the app
@@ -15,6 +17,9 @@ function App() {
         <Routes>
           <Route exact path='/' element={ <Dashboard /> } /> 
           <Route path='/project/:id' element={ <ProjectDetails/> } />
+          <Route path='/signin' element={ <SignIn />} />
+          <Route path='/signup' element={ <SignUp />} />
+          <Route path='/create' element={ <CreateProject />} />
         </Routes>
       </div>
     </BrowserRouter>
